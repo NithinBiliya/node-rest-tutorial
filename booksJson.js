e00724bb74da7f9756e848ad4fdce1c188713288
Nithin Biliya <nithin.biliya@games24x7.com>
2020-04-08 18:22:09 +0530

    initial commit with the CRUD operations

---
+++ booksJson.js
@@ -0,0 +1,44 @@
+db.books.insert([
+  {
+    title: 'exercitation',
+    genre: 'Action',
+    author: 'Rachael Leon',
+    read: true,
+  },
+  {
+    title: 'elit',
+    genre: 'Biography',
+    author: 'Simone Hewitt',
+    read: false,
+  },
+  {
+    title: 'minim',
+    genre: 'Action',
+    author: 'Anita Combs',
+    read: true,
+  },
+  {
+    title: 'laboris',
+    genre: 'Action',
+    author: 'Noble Graves',
+    read: false,
+  },
+  {
+    title: 'aliqua',
+    genre: 'Action',
+    author: 'Cobb Holmes',
+    read: false,
+  },
+  {
+    title: 'dolore',
+    genre: 'Fiction',
+    author: 'Kaye Walton',
+    read: true,
+  },
+  {
+    title: 'mollit',
+    genre: 'Fiction',
+    author: 'Maryanne Burgess',
+    read: true,
+  },
+]);
---
+++ routes/book.js
@@ -0,0 +1,81 @@
+const express = require('express');
+
+function routes(Book) {
+  const bookRouter = express.Router();
+  bookRouter
+    .route('/books')
+    .post((req, res) => {
+      const book = new Book(req.body);
+      book.save();
+      return res.status(201).json(book);
+    })
+    .get((req, res) => {
+      const query = {};
+      if (req.query.genre) {
+        query.genre = req.query.genre;
+      }
+      Book.find(query, (err, books) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(books);
+      });
+    });
+
+  bookRouter.use('/books/:id', (req, res, next) => {
+    Book.findById(req.params.id, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
+    });
+  });
+
+  bookRouter
+    .route('/books/:id')
+    .get((req, res) => res.json(req.book))
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach((item) => {
+        book[item[0]] = item[1];
+      });
+      book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.sendStatus(204);
+      });
+    });
+
+  return bookRouter;
+}
+
+module.exports = routes;
